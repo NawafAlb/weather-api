844798dd2c5f976af38a14f3eccd66c9c00c4330
k-fed <150308063+K-Fedd@users.noreply.github.com>
2025-09-29 15:22:20 -0400

    Update 03_seed.sql

--- src/db/03_seed.sql
+++ src/db/03_seed.sql
@@ -1,9 +1,9 @@
 INSERT INTO user_DataUV (latitude, longitude, uv_index)
 VALUES
-(40, -70, 2.8, 12),
-(30, -111, 7.2, 80),
-(51, 0, 3.4, 30),
-(-20, 37, 4.2, 55);
+(40, -70, 2.8),
+(30, -111, 7.2),
+(51, 0, 3.4),
+(-20, 37, 4.2);
 
 INSERT INTO user_DataAirQuality (latitude, longitude, air_quality)
 VALUES
@@ -11,4 +11,5 @@ VALUES
 (30, -109, 80),
 (53, 0, 30),
 (-20, 37, 55);
+
 
