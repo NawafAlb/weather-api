1c9da7304b62d6bd87af51f412613ebd00a165e2
k-fed <150308063+K-Fedd@users.noreply.github.com>
2025-09-29 14:37:22 -0400

    Update 01_schema.sql

--- src/db/01_schema.sql
+++ src/db/01_schema.sql
@@ -1,15 +1,15 @@
 -- Drops user_data table if it already exists
-DROP TABLE IF EXISTS user_data;
+DROP TABLE IF EXISTS user_Uvdata;
 
 -- Creates use_data table with (User ID, latitude & longitude, uv index, air quality, and the time the uv/air quality were calculated)
-CREATE TABLE user_data (
+CREATE TABLE user_Uvdata (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   latitude REAL NOT NULL,
   longitude REAL NOT NULL,
   uv_index REAL,
-  air_quality INTEGER,
 );
 
 -- Goes through the table trying to find an instance of matching latitude and longitude (Could be modified to take another variable)
-CREATE INDEX IF NOT EXISTS idx_lat_lon ON user_data (latitude, longitude);
+CREATE INDEX IF NOT EXISTS idx_lat_lon ON user_Uvdata (latitude, longitude);
+
 
