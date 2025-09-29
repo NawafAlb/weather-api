570067555933123178ffd2e4c2610e6b5c610f57
k-fed <150308063+K-Fedd@users.noreply.github.com>
2025-09-29 14:46:11 -0400

    Update and rename 02_AirQualityschema.sql to 02_schemaAirQuality.sql

--- src/db/02_schemaAirQuality.sql
+++ src/db/02_schemaAirQuality.sql
@@ -1,8 +1,8 @@
 -- Drops user_data table if it already exists
-DROP TABLE IF EXISTS user_AirQualitydata;
+DROP TABLE IF EXISTS user_DataAirQuality;
 
 -- Creates use_data table with (User ID, latitude & longitude, uv index, air quality, and the time the uv/air quality were calculated)
-CREATE TABLE user_AirQualitydata (
+CREATE TABLE user_DataAirQuality (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   latitude REAL NOT NULL,
   longitude REAL NOT NULL,
@@ -10,4 +10,4 @@ CREATE TABLE user_AirQualitydata (
 );
 
 -- Goes through the table trying to find an instance of matching latitude and longitude (Could be modified to take another variable)
-CREATE INDEX IF NOT EXISTS idx_lat_lon ON user_AirQualitydata (latitude, longitude);
+CREATE INDEX IF NOT EXISTS idx_lat_lon ON user_DataAirQuality (latitude, longitude);
