f8c90f0487af3c3813a16460e9ca3fb0be76678c
NoahStefani <ns1042788@wcupa.edu>
2025-10-03 13:46:07 -0400

    updated names

--- src/main/java/com/github/nawafalb/aqiLoader.java
+++ src/main/java/com/github/nawafalb/aqiLoader.java
@@ -17,7 +17,7 @@ import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.io.FileWriter;
 
-public class Aqiloader {
+public class AqiLoader {
     public static void main(String[] args) throws Exception {
         Dotenv env = Dotenv.load();
 
